f11445a4b82d2627877989ce65fe8a755d9f747f
Sergio Fenoll <sergio@fenoll.be>
2022-11-16 15:40:41 +0100

    feat: update relation direction between source and derived file

--- lib/file_provenance.py
+++ lib/file_provenance.py
@@ -4,17 +4,17 @@ from escape_helpers import sparql_escape_uri, sparql_escape_string, sparql_escap
 
 MU_APPLICATION_GRAPH = os.environ.get("MU_APPLICATION_GRAPH")
 
-def construct_set_file_source(new_file, source_file, graph=MU_APPLICATION_GRAPH):
+def construct_set_file_source(derived_file, source_file, graph=MU_APPLICATION_GRAPH):
     query_template = Template("""
 PREFIX prov: <http://www.w3.org/ns/prov#>
 
 INSERT DATA {
     GRAPH $graph {
-        $new_file prov:hadPrimarySource $source_file .
+        $derived_file prov:hadPrimarySource $source_file .
     }
 }
 """)
     return query_template.substitute(
         graph=sparql_escape_uri(graph),
-        new_file=sparql_escape_uri(new_file),
+        derived_file=sparql_escape_uri(derived_file),
         source_file=sparql_escape_uri(source_file))
