2e8ec0e85dae29419a4068bbe566c30ced0d778e
Sergio Fenoll <sergio@fenoll.be>
2024-01-11 16:55:44 +0100

    feat: replace service with a Microsoft Graph API implementation

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Config
-FILE_RESOURCE_BASE_URI = os.environ.get("FILE_RESOURCE_BASE_URI", "http://themis.vlaanderen.be/id/bestand")
-
-# Constants
-UNOSERVER_HOST = "127.0.0.1"
-UNOSERVER_PORT = "2002"
-JSONAPI_FILES_TYPE = "files"
