0208a329fdb81d4eda0ebc291c987b2233951fc2
Sven Valenberghs <sven.valenberghs@faros.be>
2025-03-27 11:54:50 +0100

    KAS-5020 add more error handling

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ app.post("/files/:id/convert", async (req, res, next) => {
     const stats = fs.statSync(file.path);
     file.size = stats.size;
     const readStream = fs.createReadStream(file.path);
+    
+    // If this fails this crashes the app if not caught.
+    readStream.on('error', function(err) {
+      console.log(`Error reading readStream: ${err.message}`);
+      // silence service crashing error, throwing here crashes
+      console.trace(err);
+    });
 
     await execWithRetry(() => uploadFile(file, readStream));
 
@@ -37,6 +44,7 @@ app.post("/files/:id/convert", async (req, res, next) => {
     });
   } catch (err) {
     console.log(`Error converting DOCX file: ${err.message}`);
+    console.trace(err);
     return next({ message: JSON.stringify(err.message), status: 500 });
   }
 });
